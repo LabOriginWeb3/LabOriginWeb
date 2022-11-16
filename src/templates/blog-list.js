045bb5226f6d4f3c936409018dbb5cdff762b012
cuegod <hengxingtianxia@gmail.com>
2022-11-16 12:49:15 +0800

    adjust styles

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -116,9 +116,11 @@ class BlogIndex extends React.Component {
             "labOrigin base blog page " + currentPage + " of " + numPages
           }
         />
-        <h1>Blog</h1>
-        <div className="grids col-1 sm-2 lg-3">{posts}</div>
-        <Pagination {...props} />
+        <div className="wrapper">
+          <h1>Blog</h1>
+          <div className="grids col-1 sm-2 lg-3">{posts}</div>
+          <Pagination {...props} />
+        </div>
       </Layout>
     )
   }
